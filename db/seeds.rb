278ca1ee0d82855056f3502e6f5b6bd23a4b0762
Davidalen Fountain <dfountain@sacent.com>
2018-12-29 06:18:29 +0000

    Add basic Chat function without live updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "david", password: "password")
+User.create(username: "dmitriy", password: "password")
+User.create(username: "brad", password: "password")
+User.create(username: "katie", password: "password")
+User.create(username: "noelle", password: "password")
+
+Message.create(body: "What up Dima?", user: User.find(2))
+Message.create(body: "Sup!", user: User.find(3))
+Message.create(body: "Find a lady yet?", user: User.find(4))
+Message.create(body: "Nope..... Such a disapointment", user: User.find(3))
+Message.create(body: "You guys are so dumb......", user: User.find(5))
